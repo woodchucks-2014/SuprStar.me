b01b49ccad61e80397f2ecf47729204ba6ea83a9
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-24 14:31:57 -0400

    Validating models

--- superstarme/app/models/party.rb
+++ superstarme/app/models/party.rb
@@ -2,5 +2,9 @@ class Party < ActiveRecord::Base
 
   has_many     :users
   has_many     :songs
-  
+
+  validates    :name, presence: true, uniqueness: true
+  validates    :hash_tag, presence: true, uniqueness: true
+  #validates    :queue
+
 end
--- superstarme/app/models/song.rb
+++ superstarme/app/models/song.rb
@@ -2,5 +2,9 @@ class Song < ActiveRecord::Base
 
   belongs_to  :user
   belongs_to  :party
-  
+
+  validates    :name, presence: true
+  validates    :youtube_url, presence: true
+  # validates    :lyrics
+
 end
