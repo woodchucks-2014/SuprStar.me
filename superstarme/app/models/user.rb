d55b7a973236861f34b5f934d117b046e5e77ad3
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-24 16:38:12 -0400

    model testing in progress, WIP: user phone number

--- superstarme/app/models/user.rb
+++ superstarme/app/models/user.rb
@@ -4,12 +4,13 @@ class User < ActiveRecord::Base
   belongs_to   :party
   has_many     :songs
 
-  validates    :name, presence: true
-  validates    :phone_number, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/ ,
-                           message: "Wrong Format 000-000-0000"},
-                           presence: true
+  validates_presence_of    :name, :phone_number
+  validates    :phone_number, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+                              message: "Wrong Format 000-000-0000"}
                            #might have to change this to account for the +1
 
   validates    :is_verified, presence: true
 
 end
+
+
