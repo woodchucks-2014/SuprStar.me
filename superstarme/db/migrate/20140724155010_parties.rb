611bd2f9d679529acd2123cc59c239dfa66eb0cf
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-24 13:05:10 -0400

    populating migrations, first time

--- superstarme/db/migrate/20140724155010_parties.rb
+++ superstarme/db/migrate/20140724155010_parties.rb
@@ -1,4 +1,11 @@
 class Parties < ActiveRecord::Migration
   def change
+    create_table :parties do |t|
+      t.string      :name
+      t.string      :hash_tag
+      t.text        :queue
+
+      t.timestamps
+    end
   end
 end
--- superstarme/db/migrate/20140724155016_songs.rb
+++ superstarme/db/migrate/20140724155016_songs.rb
@@ -1,4 +1,13 @@
 class Songs < ActiveRecord::Migration
   def change
+    create_table :songs do |t|
+      t.string      :name
+      t.string      :youtube_url
+      t.text        :lyrics
+      t.belongs_to  :user
+      t.belongs_to  :party
+
+      t.timestamps
+    end
   end
 end
