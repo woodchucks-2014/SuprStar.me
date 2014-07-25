ec926c69821f662d44de2eea6b3ee517605d2b53
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-24 21:40:51 -0400

    testing deploying

--- superstarme/app/controllers/notification_controller.rb
+++ superstarme/app/controllers/notification_controller.rb
@@ -22,4 +22,4 @@ class NotificationController < ApplicationController
 
 
 
-end
+end 
--- superstarme/config/routes.rb
+++ superstarme/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'notification#index'
 
-  get '/send', to: 'notification#send_back'
+  # get '/send', to: 'notification#send_back'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
