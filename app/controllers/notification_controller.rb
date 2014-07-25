aea85147609aa7d2d349616b15aa236a88514989
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-25 16:42:27 -0400

    testing response when not user

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -5,8 +5,24 @@ class NotificationController < ApplicationController
 
 	end
 
-	def receive_sms
+def receive_sms
+		text = read_first_sms
+		body = text.body.split(",")
+		name = body[1]
+		hashtag = body[0]
 
+		phone_number = text.from
+		user = User.find_by(phone_number: phone_number)
+		party = Party.find_by(hash_tag: hashtag)
+
+		what_song = "What Song?" #to be revised
+
+		if !user
+			if party
+				user = User.create(name: name, phone_number: phone_number, party: party.id)
+				send_sms(user.phone_number, what_song)
+			end
+		end
 	end
 
 	def authenticate
