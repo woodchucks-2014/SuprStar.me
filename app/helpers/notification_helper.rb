89b1e887d8e76cf9dd0b4ec3ae9e91f33d82cce8
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-25 13:46:11 -0400

    setting up notification helper

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,15 +1,3 @@
 module NotificationHelper
-
-	def send_sms(phone_number, body)  
-		twilio_token = ENV["TWILIO_API_KEY"]
-		twilio_sid = ENV["TWILIO_ACC_SID"]
-  	# real_phone_number = "+1" + phone_number.delete("-")
-  	@client = Twilio::REST::Client.new twilio_sid, twilio_token
-  	@client.account.messages.create(
-  		:from => '+18454434529',
-  		:to => phone_number,
-  		:body => body
-  	)
-  end
+	
 end
-TWILIO_API_KEY=ACbc2cca55c5535fedc04a68a5f5d6da96
