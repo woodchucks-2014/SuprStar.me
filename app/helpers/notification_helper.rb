6333c4ad21a0cff44cc91eaa34802a69c7840287
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-25 13:48:44 -0400

    adding twilio client helper method

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,3 +1,8 @@
 module NotificationHelper
-	
+	def twilio_client
+    twilio_token = ENV["TWILIO_API_KEY"]
+    twilio_sid = ENV["TWILIO_ACC_SID"]
+    client = Twilio::REST::Client.new twilio_sid, twilio_token
+	end
+
 end
