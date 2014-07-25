0717848ab3cc7d81b276bd5b357ec02b8543d400
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-24 20:22:56 -0400

    deploying to test heroku in order to test text receiving sms

--- superstarme/app/controllers/notification_controller.rb
+++ superstarme/app/controllers/notification_controller.rb
@@ -1,11 +1,33 @@
 class NotificationController < ApplicationController
 	include NotificationHelper
 
+	def index
+	end
+
 	def authenticate
 		send_sms(9099640670, 'authentication testing')
 	end
 
-	def 
+	def send_back
+		sender = params[:From]
+		friends = {
+			9099640670 => "Kenneth"
+		}
+		name = friends[sender] || "YO"
+		twiml = Twilio::TwiML::Response.new do |r|
+			r.Message "Hello, #{name}. Thanks for the message"
+		end
+		twiml.text
+	end
+
+	# def receive_sms
+	# 	reply = params[:Body]
+	# 	user_phone_number = params[:From]
+
+	# 	user = User.find_by_phone_number(user_phone_number)
+
+	# 	redirect_to :root
+	# end
 
 
 
