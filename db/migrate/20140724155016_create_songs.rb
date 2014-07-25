f84fb55606a50be163e17d3fb8bd868139e88d1d
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-25 14:16:04 -0400

    Resolving merge conflict, restructuring folders, twilio working

---
+++ db/migrate/20140724155016_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string      :name
+      t.string      :youtube_url
+      t.text        :lyrics
+      t.belongs_to  :user
+      t.belongs_to  :party
+
+      t.timestamps
+    end
+  end
+end
