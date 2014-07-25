bab959b1337925d613568e37c53f443cbe79db6a
Kai Prout <kaiprt@gmail.com>
2014-07-25 15:41:26 -0400

    Seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ActiveRecord::Base
 
-  belongs_to   :notification
+  # belongs_to   :notification
   belongs_to   :party
   has_many     :songs
 
-  validates_presence_of    :name, :phone_number
-  validates    :phone_number, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-                              message: "Wrong Format 000-000-0000"}
+  # validates_presence_of    :name, :phone_number
+  # validates    :phone_number, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+                              # message: "Wrong Format 000-000-0000"}
                            #might have to change this to account for the +1
 
-  validates    :is_verified, presence: true
+  # validates    :is_verified, presence: true
 
 end
-
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,17 @@
 #
 # Creating 2 parties
 party_1 = Party.create(name: "Cicadas", hash_tag: "#cicadas", queue: ["Iv-Xmv2yjjQ","N5IUaHr2r8Y", "0rHEpRZeh-4"])
-party_2 = Party.create(name: "Woodchucks", hash_tag: "#woodchucks", queue: ["9Xo1J9Xl0Io", "SpVNr6k5zeY", "MZxvu2sXzug"])
+# party_2 = Party.create(name: "Woodchucks", hash_tag: "#woodchucks", queue: ["9Xo1J9Xl0Io", "SpVNr6k5zeY", "MZxvu2sXzug"])
 
 # Creating users for woodchucks
-kai = User.create(name: "Kai", phone_number: "1234567890", party_id: 1)
-ken = User.create(name: "Ken", phone_number: "1234567890", party_id: 1)
-john = User.create(name: "John", phone_number: "1234567890", party_id: 1)
+p kai = User.create(name: "Kai", phone_number: "123-456-7890", party_id: 1)
+p ken = User.create(name: "Ken", phone_number: "123-456-7890", party_id: 1)
+p john = User.create(name: "John", phone_number: "123-456-7890", party_id: 1)
 
 # Creating users for cicadas
-jake = User.create(name: "Jake", phone_number: "1234567890", party_id: 2)
-jess = User.create(name: "Jess", phone_number: "1234567890", party_id: 2)
-danielle = User.create(name: "Danielle", phone_number: "1234567890", party_id: 2)
+# jake = User.create(name: "Jake", phone_number: "123-456-7890", party_id: 2)
+# jess = User.create(name: "Jess", phone_number: "123-456-7890", party_id: 2)
+# danielle = User.create(name: "Danielle", phone_number: "123-456-7890", party_id: 2)
 
 # Creating songs for woodchucks
 Song.create(name: "All of Me", youtube_url: "Iv-Xmv2yjjQ", user_id: kai.id, party_id: kai.party_id)
