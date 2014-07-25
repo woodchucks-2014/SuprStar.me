657abe9ba15ab9f3c410d1349ba06ea3714250d8
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-25 14:15:00 -0400

    preparing to resolve incoming merge

--- superstarme/config/environment.rb
+++ superstarme/config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+APP_CONG = YAML.load_file("#{RAILS_ROOT}/config/application.yml")
+
+TWILIO_ACCOUNT_SID = twilio_account_sid
+TWILIO_AUTH_TOKEN = twilio_account_sid
+TWILIO_PHONE_NUMBER = twilio_phone_number
