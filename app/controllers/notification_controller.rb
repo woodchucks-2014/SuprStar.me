911e849afe65fd39f126a35b7323e9bef2eee74a
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-25 13:45:44 -0400

    setting up notification controller

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,40 +1,5 @@
 class NotificationController < ApplicationController
 	include NotificationHelper
-
-	def index
-	end
-
-	def authenticate
-		send_sms(9099640670, 'authentication testing')
-	end
-
-	def receive_sms
-		twiml = Twilio::TwiML::Response.new do |r|
-			r.Message do |message|
-				p message.Body "Body"
-			end
-			p "*" * 40
-			render xml: "<Response/>"
-			p "*" * 40
-			# Message "Hey Monkey. Thanks for the message!"
-		end
-		twiml.text
-		# sender = params["From"]
-		# reply = params["Body"]
-		# p "*"* 150
-		# p sender
-		# p "*"* 150
-		# p reply
-		# SMSLogger.log_text_message sender, reply
-	end
-
-		# friends = {
-		# 	9099640670 => "Kenneth"
-		# }
-		# name = friends[sender] || "YO"
-		# twiml = Twilio::TwiML::Response.new do |r|
-		# 	r.Message "Hello, #{name}. Thanks for the message"
-		# end
-		# twiml.text
-
-	end 
+	
+	
+end
