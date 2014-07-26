fe164a04fb50f70a34f1b01a302bfcbd2f572a19
Kenneth M & John Quigley <pair+mendoncakr.quigproquo@devbootcamp.com>
2014-07-26 12:54:16 -0400

    new logic, saving data to database

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -29,14 +29,27 @@ class NotificationController < ApplicationController
 
 		elsif hash_tag.include?('#') && name && title_artist
 			send_sms(phone_number, get_ready_to_sing) #confirmed
+				# Save user's name, number, party_id
+				# Save song's name, user_id, party_id
+				User.create(name: name, phone_number: phone_number, party_id: party.first.id)
+				Song.create(name: title_artist, user_id: user.id, party_id: user.party.id)
+
 		elsif !hash_tag.include?('#') && name && title_artist
 			send_sms(phone_number, check_format_for_hashtag) #confirmed
+
 		elsif !array.include?(",") && hash_tag.include?('1') #comments
 			send_sms(phone_number, be_nice) #confirmed
+				# Save comment's content, user_id, party_id
+				Comment.create(content: hash_tag, user_id: user.id, party_id: user.party.id)
+
 		elsif party && hash_tag.include?("") #anything but blank
 			send_sms(phone_number, second_song) #confirmed
+
+
 		elsif
 			send_sms(phone_number, did_not_recognize)
+
+
 		end
 	end
 
