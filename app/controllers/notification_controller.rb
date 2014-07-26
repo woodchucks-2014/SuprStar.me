2eab3dcc92339ab3e2e2ec6e1b7f576621378ec3
Kenneth M & John Quigley <pair+mendoncakr.quigproquo@devbootcamp.com>
2014-07-25 20:12:49 -0400

    Completing Twilio logic, in working order

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -7,24 +7,36 @@ class NotificationController < ApplicationController
 
 	def receive_sms
 		text = read_received_sms
-		arr = text.body.split(",")
-		hash_tag = arr[0].strip
-		name = arr[1]
-
 		phone_number = text.from
+		arr = text.body.split(",")
+
 		user = User.find_by(phone_number: phone_number)
-		@party = Party.where(hash_tag: hash_tag)
 
+		if arr[0][0].include?("#")
+			hashtag = arr[0].strip
+			name = arr[1]
+			party = Party.where(hash_tag: hash_tag)
+		else
+			song_info = arr.join
+		end
 
-		what_song = "What song do you wanna sing?"
 		wrong_format = "Wrong Format. Please Try Again!
 		Format: (#hashtag, name)"
+		what_song = "You are on your way to becoming a SuprStar! What song would you like to sing?"
+		confirm_song = "You are one step closer SuprStar! Get ready to sing #{song_info}!"
 
 		if !user
-			if @party.empty?
+			if party.empty?
 				send_sms(phone_number, wrong_format)
 			else
-				user = User.create(name: name, phone_number: phone_number, party_id: @party.first.id)
+				user = User.create(name: name, phone_number: phone_number, party_id: party.first.id)
+				send_sms(user.phone_number, what_song)
+			end
+		else
+			if song_info
+				Song.create(name: song_info, user_id: user.id, party_id: user.party.id)
+				send_sms(user.phone_number, confirm_song)
+			else
 				send_sms(user.phone_number, what_song)
 			end
 		end
@@ -36,17 +48,3 @@ class NotificationController < ApplicationController
 	end
 
 end
-
-# first text should contain (hashtag, name)
-# check if phone_number is in database
-# if not user,
-	#check format of text for hashtag
-		#if hashtag exists
-			#create user with (name, phone, hashtag)
-			#send text back saying: "you've been authenticated!, what song buddy?"
-		#else
-			# send sms please specify what party you are in
-# elsif user
-	#check format of text for artist and song
-		#if artist and song exists
-			#save song to
