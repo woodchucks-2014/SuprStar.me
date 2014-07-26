dc94694ca6fcf68477d7733f50162e2c11d3f6fa
Kenneth M & John Quigley <pair+mendoncakr.quigproquo@devbootcamp.com>
2014-07-26 01:40:17 -0400

    attempting new logic, still a work in progress

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -8,37 +8,35 @@ class NotificationController < ApplicationController
 	def receive_sms
 		text = read_received_sms
 		phone_number = text.from
-		arr = text.body.split(",")
+		array = text.body.split(",")
+		welcome = text.body
+		hash_tag = array[0]
+		name = array[1]
+		title_artist = [2]
 
 		user = User.find_by(phone_number: phone_number)
+		party = Party.where(hash_tag: hash_tag)
 
-		if arr[0][0].include?("#")
-			hash_tag = arr[0].strip
-			name = arr[1]
-			party = Party.where(hash_tag: hash_tag)
-		else
-			song_info = arr.join
-		end
 
-		wrong_format = "Wrong Format. Please Try Again!
-		Format: (#hashtag, name)"
-		what_song = "You are on your way to becoming a SuprStar! What song would you like to sing?"
-		confirm_song = "You are one step closer SuprStar! Get ready to sing #{song_info} !"
 
-		if !user
-			if party.empty?
-				send_sms(phone_number, wrong_format)
-			else
-				user = User.create(name: name, phone_number: phone_number, party_id: party.first.id)
-				send_sms(user.phone_number, what_song)
-			end
-		else
-			if song_info
-				Song.create(name: song_info, user_id: user.id, party_id: user.party.id)
-				send_sms(user.phone_number, confirm_song)
-			else
-				send_sms(user.phone_number, what_song)
-			end
+		get_ready_to_sing = 'Get ready to sing SuprStar!'
+		check_format_for_hashtag = "Please try to verify party again"
+		did_not_recognize = "Try again SuprStar. example #SuprStar, Matt Bunday, Friday by Rebecca Black"
+		second_song = "Going again SuprStar?"
+		be_nice = "...."
+
+		if welcome
+
+		elsif hash_tag.include?('#') && name && title_artist
+			send_sms(phone_number, get_ready_to_sing) #confirmed
+		elsif !hash_tag.include?('#') && name && title_artist
+			send_sms(phone_number, check_format_for_hashtag) #confirmed
+		elsif !array.include?(",") && hash_tag.include?('1') #comments
+			send_sms(phone_number, be_nice) #confirmed
+		elsif party && hash_tag.include?("") #anything but blank
+			send_sms(phone_number, second_song) #confirmed
+		elsif
+			send_sms(phone_number, did_not_recognize)
 		end
 	end
 
