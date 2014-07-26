112850bca8d3b1bc3fc080c10d6a0627264b1994
Kenneth M & John Quigley <pair+mendoncakr.quigproquo@devbootcamp.com>
2014-07-25 20:18:26 -0400

    Revising Models & Migrations to reflect completion of Twilio integration

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,7 +3,6 @@ class Party < ActiveRecord::Base
   has_many     :users
   has_many     :songs
 
-  validates    :name, presence: true, uniqueness: true
   validates    :hash_tag, presence: true, uniqueness: true
   #validates    :queue
 
--- db/migrate/20140724155010_create_parties.rb
+++ db/migrate/20140724155010_create_parties.rb
@@ -1,7 +1,7 @@
 class CreateParties < ActiveRecord::Migration
   def change
     create_table :parties do |t|
-      t.string      :name
+
       t.string      :hash_tag
       t.text        :queue
 
