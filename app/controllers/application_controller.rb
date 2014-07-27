3daea2f09e7a6310867c9538c9f9a9147e0a44b3
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-27 14:48:05 -0400

    housekeeping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,36 +7,36 @@ class ApplicationController < ActionController::Base
     send_sms('845-594-2698','Hello John')
   end
 
-    YOUTUBE_DEVELOPER_KEY = ENV["YOUTUBE_API_KEY"]
-    YOUTUBE_API_SERVICE_NAME = 'youtube'
-    YOUTUBE_API_VERSION = 'v3'
+  YOUTUBE_DEVELOPER_KEY = ENV["YOUTUBE_API_KEY"]
+  YOUTUBE_API_SERVICE_NAME = 'youtube'
+  YOUTUBE_API_VERSION = 'v3'
 
-    def get_service
-      client = Google::APIClient.new(
+  def get_service
+    client = Google::APIClient.new(
       :key => YOUTUBE_DEVELOPER_KEY,
       :authorization => nil,
       :application_name => $PROGRAM_NAME,
       :application_version => '1.0.0'
       )
-      youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+    youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
-      return client, youtube
-    end
+    return client, youtube
+  end
 
-    def find(song)
+  def find(song)
 
-      client, youtube = get_service
+    client, youtube = get_service
 
-      begin
+    begin
         # Call the search.list method to retrieve results matching the specified
         # query term.
         search_response = client.execute!(
-        :api_method => youtube.search.list,
-        :parameters => {
-          :part => 'snippet',
-          :q => song + " karaoke"
-        }
-        )
+          :api_method => youtube.search.list,
+          :parameters => {
+            :part => 'snippet',
+            :q => song + " karaoke"
+          }
+          )
 
         videos = []
 
@@ -44,25 +44,25 @@ class ApplicationController < ActionController::Base
           case search_result.id.kind
           when 'youtube#video'
             videos << { :title => search_result.snippet.title,
-                        :ytid => search_result.id.videoId }
+              :ytid => search_result.id.videoId }
+            end
           end
+
+        rescue Google::APIClient::TransmissionError => e
+          puts e.result.body
         end
 
-      rescue Google::APIClient::TransmissionError => e
-        puts e.result.body
+        return videos.first
       end
 
-      return videos.first
-    end
+      def add_song(song)
+      end
 
-    def add_song(song)
+      def retrieve_from_queue
+        @party = Party.find(1)
+        p @party.queue.first
+        @next_song = @party.queue.shift
+        p @next_song
+        p @party.queue
+      end
     end
-
-    def retrieve_from_queue
-      @party = Party.find(1)
-      p @party.queue.first
-      @next_song = @party.queue.shift
-      p @next_song
-      p @party.queue
-    end
-end
