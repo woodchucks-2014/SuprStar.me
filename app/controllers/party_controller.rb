64da213ca796e68b067d917eeeacd5777cfe6fce
Kai Prout & Kenneth M <pair+kaiprt.mendoncakr@devbootcamp.com>
2014-07-27 15:13:28 -0400

    Adding method show and moving retrieve_video_id to party controller.

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,17 +1,28 @@
 class PartyController < ApplicationController
+  respond_to :json
 
-  def index
-    
+  def show
+    @party = Party.find_by_id(1)
+    @comments = Comment.where(party_id: @party.id)
   end
+
   def new
     @party = Party.new
     @user = User.new
   end
 
+  def retrieve_video_id
+    @party = Party.find_by_id(1) #where to find id?
+    @queue = @party.queue
+    @current_video = @queue.shift
+    @party.update(queue: @queue)
+
+    render json: {url: @current_video }
+  end
+
   def create
     @party = Party.new(party_params)
     @user = User.new(user_params)
-
     if @party.save && @user.save
       redirect_to 'root'
     else
