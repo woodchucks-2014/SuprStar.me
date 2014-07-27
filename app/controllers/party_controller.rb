8883651b0668b3ff6ca81ebbd61ab1b8804a4029
Kai Prout <kaiprt@gmail.com>
2014-07-26 23:13:01 -0400

    Generating controller and views.

---
+++ app/controllers/party_controller.rb
@@ -0,0 +1,25 @@
+class PartyController < ApplicationController
+  def new
+    @party = Party.new
+    @user = User.new
+  end
+
+  def create
+    @party = Party.new(party_params)
+    @user = User.new(user_params)
+
+    if @party.save && @user.save
+      redirect_to 'notification#index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def party_params
+      params.require(:party).permit(:hash_tag)
+    end
+    def user_params
+      params.require(:user).permit(:name, :phone_number)
+    end
+end
