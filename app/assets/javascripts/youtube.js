259ac555a048969210042ef7144348992101ca69
& <pair+kaiprt.mendoncakr@devbootcamp.com>
2014-07-26 20:12:12 -0400

    adding ajax calling for retrieve video ids from current party

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -1,40 +1,44 @@
+function onYouTubePlayerReady(playerId) {
+  ytplayer = document.getElementById("ytPlayer");
+  ytplayer.addEventListener("onError", "onPlayerError");
+}
 
 // Educated guess lol
 loadVideo = function(videoid) {
   var videoID = videoid //get unique id from the server
-
+  console.log(videoID);
   if (ytplayer) {
-    ytplayer.loadVideoByID(videoID); //loads by previous variable
+    ytplayer.loadVideoById(videoID); //loads by previous variable
   }
-};
+}
 
 function loadPlayer() {
-  var videoID = //id for party host (first song)
+  var videoID = 'NiXbRBS5Z58'
   var params = { allowScriptAccess: "always"};
   var atts = { id: "ytPlayer" };
   swfobject.embedSWF("http://www.youtube.com/v/" + videoID +
                      "?version=3&enablejsapi=1&playerapiid=player1",
                      "videoDiv", "640", "360", "9", null, null, params, atts);
-};
+}
 
-// Do I need this or is this for google ajax api?
-// var _run = function() {
-//   loadPlayer();
-// }
 
-(document).ready(function(){
+function _run() {
   loadPlayer();
+}
+
+$(document).ready(function(){
+  _run();
   $("#next").on("click", function(e){
     e.preventDefault();
     // Send ajax request from server getting next video id
-    // load video (function)
     $.ajax({
-      // Ajax code goes here
+      url: "/retrieve_video_id",
+      method: "GET",
+      dataType: "json"
     }).done(function( response ) {
-      loadVideo(response.videoID);
-    }.fail(function( response ){
-      console.log("Not today!")
+      loadVideo(response.url);
+    }).fail(function( response ){
+      console.log("Not today!");
     });
-    // _run with updated information
-  })
+  });
 });
