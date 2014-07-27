29cc1dafab6bd3bfbb51f07cd8afb442a2ae0e63
John Quigley <johnpeterquigley@gmail.com>
2014-07-27 13:03:32 -0400

    Testing models, working order

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Party, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "associations" do
+    it { should have_many(:users) }
+    it { should have_many(:songs) }
+  end
+
+  context "validations" do
+    it { should validate_presence_of(:hash_tag) }
+    it { should validate_uniqueness_of(:hash_tag) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
+	context "associations" do
+		it { should belong_to(:party) }
+		it { should have_many(:songs) }
+	end
+
 	context "validations" do
 		it { should validate_presence_of(:name) }
 		it { should validate_presence_of(:phone_number) }
-		it { should validate_presence_of(:is_verified) }
 	end
 end
-
