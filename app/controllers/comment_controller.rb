d3f5e67796653d2ed2bc30e0398aa18715cfaac7
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-26 22:37:25 -0400

    added index action to comment controller

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,6 +1,7 @@
 class CommentController < ApplicationController
+	
 	def index
-		@comments = Song.where("party_id = ? and created_at > ?", 1, Time.at(params[:after].to_i + 1))
+		# @comments = Comment.where("party_id = ? and created_at > ?", 1, Time.at(params[:after].to_i + 1))
 	end
-
+	
 end
