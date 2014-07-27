67bd4c673255bbee6ae9f52be714c70fc469a037
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-27 00:12:32 -0400

    added has_many comments on comment model

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,6 +3,7 @@ class Party < ActiveRecord::Base
   
   has_many     :users
   has_many     :songs
+  has_many 		 :comments
 
   validates    :hash_tag, presence: true, uniqueness: true
   #validates    :queue
