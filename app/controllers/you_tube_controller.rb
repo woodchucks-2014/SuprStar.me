7c61bc24839a02c669c97e4e376bc4306e45b851
& <pair+kaiprt.mendoncakr@devbootcamp.com>
2014-07-26 20:07:51 -0400

    adding youtube controller with retrieve video id method

--- app/controllers/you_tube_controller.rb
+++ app/controllers/you_tube_controller.rb
@@ -1,2 +1,13 @@
 class YouTubeController < ApplicationController
+  respond_to :json
+  def retrieve_video_id
+    #retrieve the first video from the current party
+    @party = Party.find_by_id(params[:id]) #where to find id?
+    @queue = @party.queue
+    @current_video = @queue.shift # returns first in line
+    @party.update(queue: @queue)
+    render json: {url: @current_video }
+  end
+
+
 end
