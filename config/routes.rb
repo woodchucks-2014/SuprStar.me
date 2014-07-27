2ab34ec56bd0feb960eaf4f3410406ad6528db81
Kai Prout <kaiprt@gmail.com>
2014-07-26 23:40:15 -0400

    changing root and added index to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'notification#index'
+  root to: 'party#index'
 
   get 'receive_sms' => "notification#receive_sms", :as => :receive_sms
-
   get 'retrieve_video_id' => "you_tube#retrieve_video_id"
 
   resources :party
