3a2476ff5601e6567dde1316f50e6fc25a981937
Kai Prout <kaiprt@gmail.com>
2014-07-28 18:43:59 -0400

    Converting JavaScript to OOJS.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,54 +1,54 @@
-function seconds(datetime) {
-  return new Date(datetime).getTime() / 1000;
-};
-
 $(function(){
   if ($("#comments").length > 0) {
-    setTimeout(updateLists, 5000);
+    setTimeout(YouTube.updateQueue, 5000);
   };
 });
 
-function updateLists () {
+var Comment = {
+  updateComments: function(){
+    var latestCommentTime = {time: $(".comment li:last-child").attr("data-time")};
+    $.ajax({
+      url: "/retrieve_comments",
+      method: "POST",
+      data: latestCommentTime
+    }).success(function(response){
+      for (var i=0; i < response.content.length; i++) {
+        var time = response.content[i].obj.created_at;
+        var time_in_seconds = seconds(time);
+        var appendLi = $('.comment').append('<li data-time="'+ time_in_seconds +'">' + response.content[i].obj.content + response.content[i].name + '</li>').fadeIn();
+        if($(".comment li").size() >= 5) {
+          $(".comment li:first-child").slideUp("slow").remove();
+        }
+        appendLi
+      };
+    }).fail(function(response){
+      console.log("No Comments yet");
+    });
+  },
 
-  var latestCommentTime = {time: $(".comment li:last-child").attr("data-time")};
-  var updateComment = $.ajax({
-    url: "/retrieve_comments",
-    method: "POST",
-    data: latestCommentTime
-  });
-  updateComment.done(function( response ) {
-    for (var i=0; i < response.content.length; i++) {
-      var time = response.content[i].obj.created_at;
-      var time_in_seconds = seconds(time);
-      if($(".comment li").size() >= 5) {
-        $(".comment li:first-child").slideUp("slow").remove();
-        $('.comment').append('<li data-time="'+ time_in_seconds +'">' + response.content[i].obj.content + response.content[i].name +'</li>').fadeIn();
-      } else {
-        $('.comment').append('<li data-time="'+ time_in_seconds +'">' + response.content[i].obj.content + response.content[i].name + '</li>').fadeIn();
+  updateQueue: function(){
+    $.ajax({
+      url: "/retrieve_queue",
+      method: "GET"
+    }).success(function(response){
+      for (var i=0; i < response.queue.length; i++) {
+        var comment = '<li>' + response.queue[i].name + '</li>';
+        if ($('.queue li:contains("'+ response.queue[i].name +'")').length < 1) {
+          $('.queue').append(comment);
+        }
       }
-    };
-  });
-  updateComment.fail(function( response ){
-    console.log("No Comments yet");
-  });
-// <--------------------------------------------------->
-  var updateQueue = $.ajax({
-    url: "/retrieve_queue",
-    method: "GET"
-  });
+    }).fail(function(response){
+      console.log("FAIL");
+    });
+  }
+};
 
-  updateQueue.done(function(response){
-    for (var i=0; i < response.queue.length; i++) {
-      var comment = '<li>' + response.queue[i].name + '</li>';
-      if ($('.queue li:contains("'+ response.queue[i].name +'")').length < 1) {
-        $('.queue').append(comment);
-      }
-    }
-  });
+var _runPolling = function() {
+  Comment.updateComments();
+  Comment.updateQueue();
+};
 
-  updateQueue.fail(function(response){
-    console.log("FAIL");
-  });
+$(document).ready(function(){
+ _runPolling();
 
-  setTimeout(updateLists, 5000);
-}
+});
--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -1,62 +1,59 @@
-function onYouTubePlayerReady(playerId) {
-  ytplayer = document.getElementById("ytPlayer");
-  ytplayer.addEventListener("onError", "onPlayerError");
+var _run = function(videoId) {
+  YouTube.loadPlayer(videoId);
 }
 
-loadVideo = function(videoid) {
-  var videoID = videoid //get unique id from the server
-  console.log(videoID);
-  if (ytplayer) {
-    ytplayer.loadVideoById(videoID); //loads by previous variable
+var YouTube = {
+  onYouTubePlayerReady: function(playerId) {
+     var ytplayer = document.getElementById("ytPlayer");
+    ytplayer.addEventListener("onError", "onPlayerError");
+  },
+  loadVideo: function(videoId) {
+    if (ytplayer) {
+      ytplayer.loadVideoById(videoID);
+    }
+  },
+  loadPlayer: function(videoID) {
+    var params = { allowScriptAccess: "always"};
+    var atts = { id: "ytPlayer" };
+    swfobject.embedSWF("http://www.youtube.com/v/" + videoID +
+    "?version=3&enablejsapi=1&playerapiid=player1",
+    "videoPlayer", "960", "540", "9", null, null, params, atts);
+  },
+  get_first_video: function() {
+    $.ajax({
+      url: "/retrieve_video_id",
+      method: "GET",
+      dataType: "json"
+    }).success(function(response){
+        _run(response.url.youtube_url);
+        $("#videoDiv").slideDown();
+      }).fail(function(response){
+        console.log("Video Failed To Load");
+    });
+  },
+
+  get_next_video: function() {
+    $.ajax({
+      url: "/retrieve_video_id",
+      method: "GET",
+      dataType: "json"
+    }).success(function(response){
+      YouTube.loadVideo(response.url.youtube_url);
+    }).fail(function(response){
+      console.log("Your video failed to load.");
+    });
   }
-}
-
-function loadPlayer(video) {
-  var videoID = video;
-  var params = { allowScriptAccess: "always"};
-  var atts = { id: "ytPlayer" };
-  swfobject.embedSWF("http://www.youtube.com/v/" + videoID +
-   "?version=3&enablejsapi=1&playerapiid=player1",
-   "videoPlayer", "960", "540", "9", null, null, params, atts);
-}
-
-function _run(videoId) {
-  loadPlayer(videoId);
-}
+};
 
 $(document).ready(function(){
   $("#videoPlayer").hide();
   $("#start").on("click", function(e){
     e.preventDefault();
     $("#start").fadeOut();
-    var get_first_video = $.ajax({
-      url: "/retrieve_video_id",
-      method: "GET",
-      dataType: "json"
-    });
-    get_first_video.success(function(response){
-    _run(response.url.youtube_url);
-      console.log(response);
-      $("#videoDiv").slideDown();
-    });
-    get_first_video.fail(function(response){
-      console.log("Video Failed To Load");
-    });
+    YouTube.get_first_video();
   });
-
   $("#next").on("click", function(e){
     e.preventDefault();
-    // Send ajax request from server getting next video id
-    $.ajax({
-      url: "/retrieve_video_id",
-      method: "GET",
-      dataType: "json"
-    }).done(function( response ) {
-
-      loadVideo(response.url.youtube_url);
-    }).fail(function( response ){
-      console.log("Not today!");
-    });
-
+    YouTube.get_next_video();
   });
 });
