81cf1fdfa67e1193062ddb0d7364d2f664ff3109
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-28 12:24:51 -0400

    pulling origin master

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,22 +1,23 @@
 class PartyController < ApplicationController
   include YouTubeHelper
   respond_to :json
-
-  def show
-    @party = Party.find(session[:party_id])
-    @comments = @party.comments
-    @queue = @queue = @party.queue
-  end
-
-  def new
+  def index
     @party = Party.new
     @user = User.new
     @song = Song.new
   end
 
+  def show
+    @party = Party.find(session[:party_id])
+    @comments = @party.comments
+    @queue = @queue = @party.queue
+
+  end
+
   def create
     @party = Party.new(party_params)
     @user = User.new(user_params)
+    @song = Song.new
     if @party.save && @user.save
       session[:party_id] = @party.id
       first = find(first_song[:name])
@@ -27,7 +28,7 @@ class PartyController < ApplicationController
       redirect_to retrieve_party_path
     else
       flash[:notice] = "Something went wrong, please try again."
-      render 'new'
+      render 'index'
     end
   end
 
