cc3d088c9c675c9cb26c9a17e5fc6f38a4877626
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-28 12:21:00 -0400

    Outlining party spec tests

--- spec/controllers/party_controller_spec.rb
+++ spec/controllers/party_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe PartyController, :type => :controller do
 
+  describe "#show" do
+    it "finds a party's session"
+    it "finds a party's comments"
+    it "finds a party's queue"
+  end
+
+  describe "#new" do
+    it "creates a new party"
+    it "creates a new user"
+    it "creates a new song"
+  end
+
+  describe "#create" do
+
+  end
+
+
 end
