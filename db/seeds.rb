343ff59ff68e685d7fb270f9b75680275d3bfa1c
Kai Prout & Kenneth M <pair+kaiprt.mendoncakr@devbootcamp.com>
2014-07-28 01:11:09 -0400

    temp seed file for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@
 # #
 # # Creating 2 parties
 # party_1 = Party.create(name: "Cicadas", hash_tag: "#cicadas", queue: ["Iv-Xmv2yjjQ","N5IUaHr2r8Y", "0rHEpRZeh-4"])
-# # party_2 = Party.create(name: "Woodchucks", hash_tag: "#woodchucks", queue: ["9Xo1J9Xl0Io", "SpVNr6k5zeY", "MZxvu2sXzug"])
+party_2 = Party.create(name: "Woodchucks", hash_tag: "#woodchucks", queue: [])
 #
 # # Creating users for woodchucks
-# p kai = User.create(name: "Kai", phone_number: "123-456-7890", party_id: 1)
-# p ken = User.create(name: "Ken", phone_number: "123-456-7890", party_id: 1)
-# p john = User.create(name: "John", phone_number: "123-456-7890", party_id: 1)
+p kai = User.create(name: "Kai", phone_number: "+11234567890", party_id: 1)
+p ken = User.create(name: "Ken", phone_number: "+12234567890", party_id: 1)
+p john = User.create(name: "John", phone_number: "+13234567890", party_id: 1)
 #
 # # Creating users for cicadas
 # # jake = User.create(name: "Jake", phone_number: "123-456-7890", party_id: 2)
@@ -16,6 +16,10 @@
 # # danielle = User.create(name: "Danielle", phone_number: "123-456-7890", party_id: 2)
 #
 # # Creating songs for woodchucks
-# Song.create(name: "All of Me", youtube_url: "Iv-Xmv2yjjQ", user_id: kai.id, party_id: kai.party_id)
-# Song.create(name: "Just Give Me A Reason", youtube_url: "N5IUaHr2r8Y", user_id: ken.id, party_id: ken.party_id)
-# Song.create(name: "Because of You", youtube_url: "0rHEpRZeh-4", user_id: john.id, party_id: john.party_id)
+party = Party.find_by_id(1)
+15.times do
+  p @temp = party.queue << Song.create(name: "All of Me", youtube_url: "Iv-Xmv2yjjQ", user_id: kai.id, party_id: kai.party_id).serializable_hash
+  p @temp = party.queue << Song.create(name: "Just Give Me A Reason", youtube_url: "N5IUaHr2r8Y", user_id: ken.id, party_id: ken.party_id).serializable_hash
+  p @temp = party.queue << Song.create(name: "Because of You", youtube_url: "0rHEpRZeh-4", user_id: john.id, party_id: john.party_id).serializable_hash
+end
+party.update(queue: @temp)
