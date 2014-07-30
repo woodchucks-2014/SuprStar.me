6fd338243204a763a2ffc3f7be417ab9135d2f00
Kai Prout <kaiprt@gmail.com>
2014-07-30 10:29:43 -0400

    Recfactoring party controller.

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -9,7 +9,7 @@ class PartyController < ApplicationController
 
   def show
     @party = Party.find(session[:party_id])
-    @comments = @party.comments
+    p @comments = @party.comments
     @queue = @queue = @party.queue
   end
 
