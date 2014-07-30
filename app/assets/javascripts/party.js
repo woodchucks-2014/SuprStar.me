8ed874ec2bf476d55fa977cc9799ce05379f2589
Kai Prout <kaiprt@gmail.com>
2014-07-29 22:32:31 -0400

    Changing backgrounds and centering some divs.

--- app/assets/javascripts/party.js
+++ app/assets/javascripts/party.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("#partyForm").hide();
-  $("#toForm").on("click", function(e){
+  $("#toForm").click(function(e){
     e.preventDefault();
     $("#toForm").hide();
     $("#partyForm").fadeIn("slow");
