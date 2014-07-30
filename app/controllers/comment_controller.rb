b0ef4ba0c100449de17a88b1f335b3d76141b5ed
Kai Prout <kaiprt@gmail.com>
2014-07-29 20:37:09 -0400

    Refactoring comment controller

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -3,13 +3,13 @@ class CommentController < ApplicationController
 	respond_to :json
 
 	def retrieve_comments
-		  @party = Party.find_by_id(session[:party_id]) #where to get party id from?
-		  @comments = @party.comments.where("created_at > ?", Time.at(params[:time].to_i + 1))
-		  @new_comments = []
-		  @comments.each do |com|
-			hash = {name: com.user.name, obj: com}
-			@new_comments << hash
-		end
+		  party = Party.find_by_id(current_party) #where to get party id from?
+		  comments = party.comments.where("created_at > ?", Time.at(params[:time].to_i + 1))
+		  new_comments = []
+		  comments.each do |com|
+				hash = {name: com.user.name, obj: com}
+				@new_comments << hash
+			end
 		render json: {content: @new_comments}.to_json
 	end
 
