3290dfaac52bb8f9f62b34c7377e8da017c5ad36
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-31 03:31:55 -0400

    working comments

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -8,9 +8,8 @@ class CommentController < ApplicationController
 		@new_comments = []
 		@sentimental_score = 0
 		@comments.each do |com|
-			com.analyze_sentiment
 			hash = {name: com.user.name, obj: com}
-			@sentimental_score += (1.5 * 5) #do math here
+			@sentimental_score += (com.score * 5) #do math here
 			@new_comments << hash
 		end
 		render json: {content: @new_comments, sentimental_score: @sentimental_score}.to_json
