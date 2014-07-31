112edb5f2d5c26a77c55b983bf795d56514263c1
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-31 03:12:47 -0400

    changing @party

--- db/migrate/20140726163700_create_comments.rb
+++ db/migrate/20140726163700_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-    	t.float 			:score
+    	t.float 			:score, default: 0
       t.string      :content
       t.belongs_to  :party
       t.belongs_to  :user
