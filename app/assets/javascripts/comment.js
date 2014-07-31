745452edc7c968529bb2acde2b3f6ad8c1bb5475
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-31 19:57:34 -0400

    Commented out overlapping jquery event.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -18,8 +18,8 @@ var Comment = {
     }).success(function(response){
       // console.log(response.content);
       console.log(response.sentimental_score);
-      $('meter').data('value', (50 + response.sentimental_score));
-      $("#booMeter").attr("value", '"'+ response.sentimental_score + '"');
+      // $('meter').data('value', (50 + response.sentimental_score));
+      $("#booMeter").attr("value", '"'+ 50 + response.sentimental_score + '"');
       for (var i=0; i < response.content.length; i++) {
         var time = response.content[i].obj.created_at;
         var time_in_seconds = seconds(time);
