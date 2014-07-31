ddb829091e6dd42b58d7e9397b77c07b2ad4e444
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-31 11:05:00 -0400

    changing text in views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :party
-  before_create :analyze_sentiment
 
   def analyze_sentiment
     results = AlchemyAPI.search(:sentiment_analysis, :text => self.content)
