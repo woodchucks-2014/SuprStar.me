ada63ef964f15851d9a2dd4f0511a518e26bdffb
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-31 01:25:47 -0400

    changing comment score datatype to float

--- db/migrate/20140726163700_create_comments.rb
+++ db/migrate/20140726163700_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string      :content
-      t.integer     :score
+      t.float     :score
       t.belongs_to  :party
       t.belongs_to  :user
 
